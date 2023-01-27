5799efb7bae64d5709c634a9793c92cbd0b2b59e
sergeybryan <regkmastis@yandex.ru>
2023-01-27 18:08:13 +0300

    Начало работы над Домашней работой №3

---
+++ src/main/java/com/example/web_homework3/WebHomeWork3Application.java
@@ -0,0 +1,13 @@
+package com.example.web_homework3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebHomeWork3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebHomeWork3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/web_homework3/WebHomeWork3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_homework3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebHomeWork3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
